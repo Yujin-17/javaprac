058101d024c871cb4c3330af933674443da12996
yujin-17 <lyjyellow@gmail.com>
2023-02-21 13:50:01 +0900

    230221 객체지향

---
+++ chap01/chap01_1/CallStack5_1.java
@@ -0,0 +1,27 @@
+package chap01_1;
+
+class CallStack5_1 {
+
+    static void firstMethod() {
+        System.out.println("firstMethod()");
+        secondMethod();
+    }
+
+    static void secondMethod() {
+        System.out.println("secondMethod()");
+        thirdMethod();
+    }
+
+    static void thirdMethod() {
+        System.out.println("thirdMethod()");
+        finalMethod();
+    }
+
+    static void finalMethod(){
+        System.out.println("finalMethod()");
+    }
+
+    public static void main(String[] args) {
+        firstMethod();
+    }
+}
---
+++ chap01/chap01_1/CallStack5_2.java
@@ -0,0 +1,27 @@
+package chap01_1;
+
+class CallStack5_2 {
+
+    static void firstMethod() {
+        secondMethod();
+        System.out.println("firstMethod()");
+    }
+
+    static void secondMethod() {
+        thirdMethod();
+        System.out.println("secondMethod()");
+    }
+
+    static void thirdMethod() {
+        finalMethod();
+        System.out.println("thirdMethod()");
+    }
+
+    static void finalMethod(){
+        System.out.println("finalMethod()");
+    }
+
+    public static void main(String[] args) {
+        firstMethod();
+    }
+}
